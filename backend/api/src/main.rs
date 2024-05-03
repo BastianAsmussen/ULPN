808b6a6b2e4dde317fe40c7a59fd66a7252afcbd
Bastian Asmussen <bastian@asmussen.tech>
2024-05-03 08:29:46 +0200

    Create API project.

---
+++ backend/api/src/main.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use axum::Router;
+
+use tracing::debug;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+
+    debug!("Listening on {}.", listener.local_addr()?);
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
