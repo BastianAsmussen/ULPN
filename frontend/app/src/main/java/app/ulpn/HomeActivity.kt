8181990a23b8153b0e7b4df5f524b946b316d01c
Zelvios <jaco296c@outlook.dk>
2024-05-06 10:37:02 +0200

    Updated imports.

--- frontend/app/src/main/java/app/ulpn/HomeActivity.kt
+++ frontend/app/src/main/java/app/ulpn/HomeActivity.kt
@@ -2,8 +2,6 @@ package app.ulpn
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
