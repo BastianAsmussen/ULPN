49157292c54b4be71a7c3424b5046277aecf4f67
Bastian Asmussen <bastian@asmussen.tech>
2024-05-06 10:15:13 +0200

    Add dotenvy.

--- backend/api/src/main.rs
+++ backend/api/src/main.rs
@@ -3,10 +3,14 @@ use axum::Router;
 
 use tracing::debug;
 
+mod db;
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenvy::dotenv()?;
     tracing_subscriber::fmt::init();
 
+    let pool = db::init().await?;
     let app = Router::new();
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
