352738014b8f020b89f4198f68c9cb1c4a6dfaa6
Bastian Asmussen <bastian@asmussen.tech>
2024-05-06 09:59:22 +0200

    Update schema.

--- backend/api/src/schema.rs
+++ backend/api/src/schema.rs
@@ -10,11 +10,56 @@ diesel::table! {
     use diesel::sql_types::*;
     use super::sql_types::AccessLevel;
 
-    users (id) {
+    forums (id) {
+        id -> Int4,
+        #[max_length = 256]
+        name -> Varchar,
+        access_level -> AccessLevel,
+    }
+}
+
+diesel::table! {
+    identities (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+    }
+}
+
+diesel::table! {
+    messages (id) {
         id -> Int8,
+        forum_id -> Int4,
+        sender_id -> Int4,
+        reply_id -> Int8,
+        identity -> Int4,
+        contents -> Text,
+        created_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::AccessLevel;
+
+    users (id) {
+        id -> Int4,
         #[max_length = 8]
         unilogin -> Bpchar,
+        #[max_length = 1024]
+        full_name -> Varchar,
         access_level -> AccessLevel,
         is_admin -> Bool,
     }
 }
+
+diesel::joinable!(messages -> forums (forum_id));
+diesel::joinable!(messages -> identities (identity));
+diesel::joinable!(messages -> users (sender_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    forums,
+    identities,
+    messages,
+    users,
+);
