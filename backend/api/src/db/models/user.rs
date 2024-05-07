8d958f5a13763cfbd20c8738a0751eec63a1dba7
Bastian Asmussen <bastian@asmussen.tech>
2024-05-07 09:17:14 +0200

    Create user model.

---
+++ backend/api/src/db/models/user.rs
@@ -0,0 +1,80 @@
+use diesel::prelude::*;
+use diesel_async::{AsyncPgConnection, RunQueryDsl};
+
+use crate::db::schema::users;
+
+use anyhow::Result;
+
+#[derive(Debug, diesel_derive_enum::DbEnum)]
+#[ExistingTypePath = "crate::db::schema::sql_types::AccessLevel"]
+pub enum AccessLevel {
+    Child,
+    Parent,
+    Professional,
+    Administrator,
+}
+
+#[derive(Debug, Queryable, Selectable, Identifiable)]
+#[diesel(table_name = users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct User {
+    pub id: i32,
+
+    pub unilogin: String,
+    pub full_name: String,
+
+    pub access_level: AccessLevel,
+}
+
+impl User {
+    pub async fn by_id(conn: &mut AsyncPgConnection, id: i32) -> Result<Self> {
+        use crate::db::schema::users::dsl::users;
+        
+        let result = users
+            .find(id)
+            .select(Self::as_select())
+            .first(conn)
+            .await?;
+
+        Ok(result)
+    }
+
+    pub async fn by_unilogin(conn: &mut AsyncPgConnection, value: &str) -> Result<Self> {
+        use crate::db::schema::users::dsl::{unilogin, users};
+        
+        let result = users
+            .filter(unilogin.eq(value))
+            .select(Self::as_select())
+            .first(conn)
+            .await?;
+
+        Ok(result)
+    }
+
+    pub async fn by_name(conn: &mut AsyncPgConnection, value: &str, limit: i64) -> Result<Vec<Self>> {
+        use crate::db::schema::users::dsl::{full_name, users};
+        
+        let results = users
+            .filter(full_name.like(value))
+            .select(Self::as_select())
+            .limit(limit)
+            .load(conn)
+            .await?;
+
+        Ok(results)
+    }
+
+    pub async fn by_access_level(conn: &mut AsyncPgConnection, value: &AccessLevel, limit: i64) -> Result<Vec<Self>> {
+        use crate::db::schema::users::dsl::{access_level, users};
+        
+        let results = users
+            .filter(access_level.eq(value))
+            .select(Self::as_select())
+            .limit(limit)
+            .load(conn)
+            .await?;
+
+        Ok(results)
+    }
+}
+
