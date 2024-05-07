2ec675161a2bfef7ba88cb701443a41e9a777444
Bastian Asmussen <bastian@asmussen.tech>
2024-05-07 09:14:25 +0200

    Made messages delete on cascade.

--- backend/api/migrations/2024-05-06-075725_create_messages/up.sql
+++ backend/api/migrations/2024-05-06-075725_create_messages/up.sql
@@ -1,13 +1,14 @@
 CREATE TABLE messages (
     id BIGSERIAL PRIMARY KEY,
 
-    forum_id SERIAL REFERENCES forums(id),
-    sender_id SERIAL REFERENCES users(id),
+    forum_id SERIAL REFERENCES forums(id) ON DELETE CASCADE,
+    sender_id SERIAL REFERENCES users(id) ON DELETE CASCADE,
 
-    reply_id BIGSERIAL REFERENCES messages(id), -- Is this a reply to something?
+    reply_id BIGSERIAL REFERENCES messages(id) ON DELETE CASCADE, -- Is this a reply to something?
 
-    identity SERIAL REFERENCES identities(id),
+    identity_id SERIAL REFERENCES identities(id) ON DELETE CASCADE,
     contents TEXT NOT NULL,
+    is_published BOOLEAN NOT NULL DEFAULT FALSE, -- Can others see this yet?
 
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
