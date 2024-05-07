98cbc2c1dcb2c05c626c2869555f5115b461f857
Bastian Asmussen <bastian@asmussen.tech>
2024-05-07 09:13:37 +0200

    Merge forums and topics.

--- backend/api/migrations/2024-05-06-073234_create_forums/up.sql
+++ backend/api/migrations/2024-05-06-073234_create_forums/up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE forums (
     id SERIAL PRIMARY KEY,
 
-    name VARCHAR(256) NOT NULL UNIQUE,
+    title VARCHAR(128) NOT NULL UNIQUE,
+    description TEXT NOT NULl, -- In markdown format.
+
     access_level access_level NOT NULL
 );
 
--- backend/api/migrations/2024-05-06-102941_create_topics/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE topics (
-    id SERIAL PRIMARY KEY,
-    
-    title VARCHAR(128) NOT NULL,
-    content TEXT NOT NULL
-);
-
