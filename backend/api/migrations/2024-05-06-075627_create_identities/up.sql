08ba77a3d937a3d6a2097ec8df72d7e05bda3ae4
Bastian Asmussen <bastian@asmussen.tech>
2024-05-23 10:27:07 +0200

    Update identities.

--- backend/api/migrations/2024-05-06-075627_create_identities/up.sql
+++ backend/api/migrations/2024-05-06-075627_create_identities/up.sql
@@ -1,5 +1,8 @@
 CREATE TABLE identities (
     id SERIAL PRIMARY KEY,
 
-    value VARCHAR(255) NOT NULL
+    owner_id SERIAL REFERENCES users(id) ON DELETE CASCADE,
+    forum_id SERIAL REFERENCES forums(id) ON DELETE CASCADE,
+
+    name VARCHAR(255) NOT NULL
 );
