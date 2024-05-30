3d3468cc017264f1e4c60e9d44ac4f46c2f14a43
Bastian Asmussen <bastian@asmussen.tech>
2024-05-30 13:18:40 +0200

    Remove dead code.

--- backend/api/src/routes/user.rs
+++ backend/api/src/routes/user.rs
@@ -1,11 +1,1 @@
-use actix_session::Session;
-use actix_web::get;
-use actix_web::web::Data;
-use crate::state::App;
 
-#[get("/login")]
-pub async fn login(session: Session, data: Data<App>) -> impl actix_web::Responder {
-    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
-    
-    todo!()
-}
