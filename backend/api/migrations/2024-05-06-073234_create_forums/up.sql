c55104a093fd4d51d8794879a8a4431801879532
Bastian Asmussen <bastian@asmussen.tech>
2024-06-09 19:06:51 +0200

    Change default lock state.

--- backend/api/migrations/2024-05-06-073234_create_forums/up.sql
+++ backend/api/migrations/2024-05-06-073234_create_forums/up.sql
@@ -4,7 +4,7 @@ CREATE TABLE forums (
     title VARCHAR(128) NOT NULL UNIQUE,
     description TEXT NOT NULl, -- In markdown format.
 
-    is_locked BOOLEAN NOT NULL DEFAULT FALSE, -- If the forum is locked, no new messages can be sent.
+    is_locked BOOLEAN NOT NULL DEFAULT TRUE, -- If the forum is locked, no messages can be sent.
 
     access_level access_level NOT NULL
 );
